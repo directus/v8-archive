27f7647def98b67f3498fd08efa31d2074916f93
Welling Guzman <hola@wellingguzman.com>
2017-12-24 03:48:03 -0500

    line by line

--- tests/bin/install_apache.sh
+++ tests/bin/install_apache.sh
@@ -8,12 +8,12 @@ ls      ~/.phpenv/versions/$(phpenv version-name)/etc
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/www.conf
 ls ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d
 sudo a2enmod rewrite actions fastcgi alias
-echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
-sudo chown -R travis:travis /var/lib/apache2/fastcgi
-~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+# echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
+# sudo chown -R travis:travis /var/lib/apache2/fastcgi
+# ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # configure apache virtual hosts
-sudo cp -f tests/assets/travis-ci-apache /etc/apache2/sites-available/000-default.conf
-sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
+# sudo cp -f tests/assets/travis-ci-apache /etc/apache2/sites-available/000-default.conf
+# sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
 sudo service apache2 restart
