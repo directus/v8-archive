f04c6e91c3b152842bb8cad4e5a79b42a80b6d95
Welling Guzman <hola@wellingguzman.com>
2017-12-23 22:35:35 -0500

    update env variables

--- tests/bin/directus_install.sh
+++ tests/bin/directus_install.sh
@@ -1,9 +1,9 @@
 npm install
 npm install -g karma-cli
 composer install
-docker run --NAME mysql-55 -p 127.0.0.1:8806:3306 \
+docker run --name mysql -p 127.0.0.1:8806:3306 \
            -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=directus -e MYSQL_USER=directus \
-           -e MYSQL_PASSWORD=directus --name mysql -d mysql:5.5
+           -e MYSQL_PASSWORD=directus -d mysql:$MYSQL_VERSION
 bin/directus install:config -h "127.0.0.1" -P 8806 -n "$DB_NAME" -u "$DIRECTUS_DB_USER" -p "$DIRECTUS_DB_PASSWORD" -d "$DIRECTUS_PATH" -e "$DIRECTUS_ADMIN_EMAIL"
 bin/directus install:database
 bin/directus install:install -e "$DIRECTUS_ADMIN_EMAIL" -p "$DIRECTUS_ADMIN_PASSWORD" -t "$DIRECTUS_SITE_NAME"
