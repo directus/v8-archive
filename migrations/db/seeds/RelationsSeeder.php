11ab9577c63145281a08dadea1ad3d1df3438a52
Welling Guzman <hola@wellingguzman.com>
2018-04-10 08:12:34 -0400

    add system relations data
    Closes #53

---
+++ migrations/db/seeds/RelationsSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class RelationsSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'collection_a' => 'directus_activity',
+                'field_a' => 'user',
+                'collection_b' => 'directus_users'
+            ],
+            [
+                'collection_a' => 'directus_activity_read',
+                'field_a' => 'user',
+                'collection_b' => 'directus_users'
+            ],
+            [
+                'collection_a' => 'directus_activity_read',
+                'field_a' => 'activity',
+                'collection_b' => 'directus_activity'
+            ],
+            [
+                'collection_a' => 'directus_collections_presets',
+                'field_a' => 'user',
+                'collection_b' => 'directus_users'
+            ],
+            [
+                'collection_a' => 'directus_collections_presets',
+                'field_a' => 'group',
+                'collection_b' => 'directus_groups'
+            ],
+            [
+                'collection_a' => 'directus_files',
+                'field_a' => 'upload_user',
+                'collection_b' => 'directus_users'
+            ],
+            [
+                'collection_a' => 'directus_files',
+                'field_a' => 'folder',
+                'collection_b' => 'directus_folders'
+            ],
+            [
+                'collection_a' => 'directus_folders',
+                'field_a' => 'parent_folder',
+                'collection_b' => 'directus_folders'
+            ],
+            [
+                'collection_a' => 'directus_permissions',
+                'field_a' => 'group',
+                'collection_b' => 'directus_groups'
+            ],
+            [
+                'collection_a' => 'directus_revisions',
+                'field_a' => 'activity',
+                'collection_b' => 'directus_activity'
+            ],
+            [
+                'collection_a' => 'directus_users',
+                'field_a' => 'group',
+                'collection_b' => 'directus_groups'
+            ],
+            [
+                'collection_a' => 'directus_users',
+                'field_a' => 'avatar',
+                'collection_b' => 'directus_files'
+            ]
+        ];
+
+        $files = $this->table('directus_files');
+        $files->insert($data)->save();
+    }
+}
