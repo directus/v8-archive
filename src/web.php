838ee88f4cddba530776f28057f75bc774f377da
Welling Guzman <hola@wellingguzman.com>
2018-06-12 09:31:24 -0400

    update http middleware namespace

--- src/web.php
+++ src/web.php
@@ -74,43 +74,43 @@ $app->getContainer()->get('hook_emitter')->run('application.boot', $app);
 //
 // ----------------------------------------------------------------------------
 
-$app->add(new \Directus\Application\Http\Middlewares\TableGatewayMiddleware($app->getContainer()))
+$app->add(new \Directus\Application\Http\Middleware\TableGatewayMiddleware($app->getContainer()))
     ->add(new RKA\Middleware\IpAddress())
-    ->add(new \Directus\Application\Http\Middlewares\CorsMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\CorsMiddleware($app->getContainer()));
 
 $app->get('/', \Directus\Api\Routes\Home::class);
 
 $app->group('/{env}', function () {
     $this->group('/activity', \Directus\Api\Routes\Activity::class)
-         ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+         ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/auth', \Directus\Api\Routes\Auth::class);
     $this->group('/fields', \Directus\Api\Routes\Fields::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/files', \Directus\Api\Routes\Files::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/items', \Directus\Api\Routes\Items::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/collection_presets', \Directus\Api\Routes\CollectionPresets::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/permissions', \Directus\Api\Routes\Permissions::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/relations', \Directus\Api\Routes\Relations::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/revisions', \Directus\Api\Routes\Revisions::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/roles', \Directus\Api\Routes\Roles::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/settings', \Directus\Api\Routes\Settings::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/collections', \Directus\Api\Routes\Collections::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/users', \Directus\Api\Routes\Users::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
     $this->group('/scim', function () {
         $this->group('/v2', \Directus\Api\Routes\ScimTwo::class);
-    })->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));;
+    })->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));;
     $this->group('/utils', \Directus\Api\Routes\Utils::class)
-        ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+        ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
 
     $this->group('/custom', function () {
         $endpointsList = get_custom_endpoints('/public/custom/endpoints');
@@ -126,7 +126,7 @@ $app->group('/{env}', function () {
         foreach ($endpointsList as $name => $endpoints) {
             create_group_route_from_array($this, $name, $endpoints);
         }
-    })->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+    })->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
 
     $this->group('/interfaces', function () {
         $endpointsList = get_custom_endpoints('public/extensions/core/interfaces', true);
@@ -134,19 +134,19 @@ $app->group('/{env}', function () {
         foreach ($endpointsList as $name => $endpoints) {
             create_group_route_from_array($this, $name, $endpoints);
         }
-    })->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($this->getContainer()));
+    })->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($this->getContainer()));
 });
 
 $app->group('/interfaces', \Directus\Api\Routes\Interfaces::class)
-    ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($app->getContainer()));
 $app->group('/listings', \Directus\Api\Routes\Listings::class)
-    ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($app->getContainer()));
 $app->group('/pages', \Directus\Api\Routes\Pages::class)
-    ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($app->getContainer()));
 $app->group('/server', \Directus\Api\Routes\Server::class)
-    ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($app->getContainer()));
 $app->group('/types', \Directus\Api\Routes\Types::class)
-    ->add(new \Directus\Application\Http\Middlewares\AuthenticationMiddleware($app->getContainer()));
+    ->add(new \Directus\Application\Http\Middleware\AuthenticationMiddleware($app->getContainer()));
 
 $app->add(new \Directus\Application\Http\Middleware\ResponseCacheMiddleware($app->getContainer()));
 // $app->add(new \Directus\Slim\HttpCacheMiddleware());
