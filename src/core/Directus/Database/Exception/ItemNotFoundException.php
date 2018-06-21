cf86fcf657bcda0c83077ced9c3ba181d4cc7351
Welling Guzman <hola@wellingguzman.com>
2018-06-21 14:45:16 -0400

    add default item not found error message

--- src/core/Directus/Database/Exception/ItemNotFoundException.php
+++ src/core/Directus/Database/Exception/ItemNotFoundException.php
@@ -8,8 +8,12 @@ class ItemNotFoundException extends NotFoundException
 {
     const ERROR_CODE = 203;
 
-    public function __construct($message = '')
+    public function __construct($message = null)
     {
+        if (!$message) {
+            $message = 'Item not found';
+        }
+
         parent::__construct($message, static::ERROR_CODE);
     }
 }
