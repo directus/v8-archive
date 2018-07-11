8cdcf49a7ed2c0fb387beaf9cc86bd3b19abd725
jsguru-git <i.asan825.mail@gmail.com>
2018-07-11 04:12:18 +0300

    1. add date formate option into calendar interface. 2. add relative date option into calendar interface.

--- extensions/core/interfaces/calendar/readonly.vue
+++ extensions/core/interfaces/calendar/readonly.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="no-wrap">{{ value }}</div>
+  <v-timeago
+    v-if="value && options.showRelative"
+    :since="date"
+    :auto-update="86400"
+    :locale="$i18n.locale"
+    class="no-wrap"
+  ></v-timeago>
+  <div v-else>{{displayValue}}</div>
 </template>
 
 <script>
@@ -7,5 +14,20 @@ import mixin from '../../../mixins/interface';
 
 export default {
   mixins: [mixin],
+  computed: {
+    date() {
+      if (this.value) {
+        return new Date(this.value.replace(/-/g, '/'));
+      }
+      return null;
+    },
+    displayValue() {
+      if (this.value && this.options.localized) {
+        return this.$d(this.date, 'short');
+      }
+
+      return this.value;
+    },
+  },
 };
 </script>
