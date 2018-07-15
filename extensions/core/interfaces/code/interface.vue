721fa16069b9adc645c6389299df9eadd9a2d968
rijkvanzanten <rijkvanzanten@me.com>
2018-07-15 15:46:10 +0200

    Autofix code style issues

--- extensions/core/interfaces/code/interface.vue
+++ extensions/core/interfaces/code/interface.vue
@@ -17,54 +17,53 @@
 </template>
 
 <script>
-
-import { codemirror } from 'vue-codemirror';
-import 'codemirror/lib/codemirror.css'
+import { codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
 
 // languages
-import 'codemirror/mode/javascript/javascript.js'
-import 'codemirror/mode/vue/vue.js'
-import 'codemirror/mode/php/php.js'
+import "codemirror/mode/javascript/javascript.js";
+import "codemirror/mode/vue/vue.js";
+import "codemirror/mode/php/php.js";
 
 // import active-line.js
-import'codemirror/addon/selection/active-line.js'
+import "codemirror/addon/selection/active-line.js";
 
 // styleSelectedText - (styleSelectedText and css class .CodeMirror-selectedtext)
-import'codemirror/addon/selection/mark-selection.js'
-import'codemirror/addon/search/searchcursor.js'
+import "codemirror/addon/selection/mark-selection.js";
+import "codemirror/addon/search/searchcursor.js";
 
 // hint - (extraKeys and hintOptions)
-import'codemirror/addon/hint/show-hint.js'
-import'codemirror/addon/hint/show-hint.css'
-import'codemirror/addon/hint/javascript-hint.js'
-import'codemirror/addon/selection/active-line.js'
+import "codemirror/addon/hint/show-hint.js";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/javascript-hint.js";
+import "codemirror/addon/selection/active-line.js";
 
 // highlightSelectionMatches - (highlightSelectionMatches)
-import'codemirror/addon/scroll/annotatescrollbar.js'
-import'codemirror/addon/search/matchesonscrollbar.js'
-import'codemirror/addon/search/searchcursor.js'
-import'codemirror/addon/search/match-highlighter.js'
+import "codemirror/addon/scroll/annotatescrollbar.js";
+import "codemirror/addon/search/matchesonscrollbar.js";
+import "codemirror/addon/search/searchcursor.js";
+import "codemirror/addon/search/match-highlighter.js";
 
 // keyMap - (matchBrackets)
-import'codemirror/mode/clike/clike.js'
-import'codemirror/addon/edit/matchbrackets.js'
-import'codemirror/addon/comment/comment.js'
-import'codemirror/addon/dialog/dialog.js'
-import'codemirror/addon/dialog/dialog.css'
-import'codemirror/addon/search/searchcursor.js'
-import'codemirror/addon/search/search.js'
-import'codemirror/keymap/sublime.js'
+import "codemirror/mode/clike/clike.js";
+import "codemirror/addon/edit/matchbrackets.js";
+import "codemirror/addon/comment/comment.js";
+import "codemirror/addon/dialog/dialog.js";
+import "codemirror/addon/dialog/dialog.css";
+import "codemirror/addon/search/searchcursor.js";
+import "codemirror/addon/search/search.js";
+import "codemirror/keymap/sublime.js";
 
-import './code.css';
-import mixin from '../../../mixins/interface';
+import "./code.css";
+import mixin from "../../../mixins/interface";
 
 export default {
   name: "interface-code",
   mixins: [mixin],
   components: {
-    codemirror,
+    codemirror
   },
-  data () {
+  data() {
     return {
       line_counts: 0,
       lang_type: null,
@@ -74,8 +73,8 @@ export default {
         "text/x-vue": "vue",
         "application/x-httpd-php": "php"
       },
-      code: null,   
-    }
+      code: null
+    };
   },
   mounted() {
     this.setEditorSize(this.codemirror, this.options);
@@ -83,7 +82,7 @@ export default {
     this.lang_type = this.avail_types[this.options.mode];
   },
   watch: {
-    options: function (options) {
+    options: function(options) {
       this.setEditorSize(this.codemirror, options);
       this.codemirror.setOption("mode", options.mode);
       this.lang_type = this.avail_types[options.mode];
@@ -99,13 +98,13 @@ export default {
         indentUnit: 4,
         styleActiveLine: true,
         lineNumbers: this.options.lineNumber,
-        readOnly: this.readonly? "nocursor" : false,
+        readOnly: this.readonly ? "nocursor" : false,
         styleSelectedText: true,
         line: true,
-       highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
+        highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
         mode: this.options.mode,
         // hint.js options
-        hintOptions:{
+        hintOptions: {
           // Automatically complete when there is only one match
           completeSingle: true
         },
@@ -114,9 +113,9 @@ export default {
         matchBrackets: true,
         showCursorWhenSelecting: true,
         theme: "default",
-        extraKeys: { "Ctrl": "autocomplete" }
-      }
-    },
+        extraKeys: { Ctrl: "autocomplete" }
+      };
+    }
   },
   methods: {
     onCmInput(newCode) {
@@ -124,27 +123,27 @@ export default {
       this.setEditorSize(this.codemirror, this.options);
       // Get line counts of the code editor
       this.line_counts = this.codemirror.lineCount();
-      this.$emit('input', newCode);
+      this.$emit("input", newCode);
     },
     onBtnClick() {
       this.code = this.options.placeholder;
     },
     setEditorSize(cm, opts) {
-      if ( opts.max != null || opts.min != null ) {
+      if (opts.max != null || opts.min != null) {
         cm.setSize("100%", "auto");
         let max = opts.max,
-            min = opts.min,
-            height = cm.getWrapperElement().offsetHeight;
+          min = opts.min,
+          height = cm.getWrapperElement().offsetHeight;
         if (min > height) {
           cm.setSize("100%", min);
-        } else if ( max < height ) {
+        } else if (max < height) {
           cm.setSize("100%", max);
         } else {
           cm.setSize("100%", "auto");
         }
       }
     }
-  },
+  }
 };
 </script>
 
