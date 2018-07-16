d028af4aa277c7083d475a7943e4f9da3225c23f
rijkvanzanten <rijkvanzanten@me.com>
2018-07-16 20:12:07 +0200

    Add color palette picker interface
    Squashed commit of the following:
    
    commit 097f79f0ef44f59abf104323a1fa88094f4d319f
    Author: rijkvanzanten <rijkvanzanten@me.com>
    Date:   Mon Jul 16 20:10:45 2018 +0200
    
        Disable hover state transition-in
    
    commit 63c04a4df475e62bd7d9763d4ed74725d811058d
    Author: Vincent Kempers <me@vncn.tk>
    Date:   Mon Jul 16 20:04:54 2018 +0200
    
        changed the animation on the in and left the out
    
    commit a4a45b48e46e32b1adfb7f8b217597630f7631eb
    Author: Vincent Kempers <me@vncn.tk>
    Date:   Mon Jul 16 19:49:10 2018 +0200
    
        add the flex grow on the last two items.
    
    commit e2ba9dff9917999df0d540a5eadf77eeaf054c7b
    Merge: 288774641 08ee2d7cb
    Author: Vincent Kempers <me@vncn.tk>
    Date:   Mon Jul 16 19:15:05 2018 +0200
    
        Merge branch 'color-picker-interface' of https://github.com/VincentKempers/api into color-picker-interface
    
    commit 2887746413587777f66f020ed72a079901d4e3f1
    Author: Vincent Kempers <me@vncn.tk>
    Date:   Mon Jul 16 19:11:27 2018 +0200
    
        rotate the picker to look more like material.io
    
    commit 08ee2d7cb97d11a7c81e83e5a64c97170806e79c
    Author: rijkvanzanten <rijkvanzanten@me.com>
    Date:   Mon Jul 16 18:13:59 2018 +0200
    
        Tiny style fixes
    
    commit d5eba6f3d6cd6f03b477fc2081be210f01cd9723
    Author: Vincent Kempers <me@vncn.tk>
    Date:   Mon Jul 16 18:07:29 2018 +0200
    
        add color picker interface

---
+++ extensions/core/interfaces/color-picker/interface.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="interface-color-picker">
+      <button
+        v-for="color in colors"
+        v-tooltip="$helpers.formatTitle(color)"
+        :key="color"
+        :style="{ backgroundColor: `var(--${color})`}"
+        :class="{ active: value === color}"
+        @click="$emit('input', color)">
+        <template
+          v-if="value === color">
+          <i class="material-icons">check</i>
+        </template>
+      </button>
+    </div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+import colors from "./colors.json";
+
+export default {
+  mixins: [mixin],
+  computed: {
+    colors() {
+      return colors;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.interface-color-picker {
+  --swatch-size: 20px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: calc(var(--swatch-size) * 11);
+  width: var(--swatch-size);
+
+  button {
+    flex-basis: var(--swatch-size);
+    width: var(--swatch-size);
+    height: var(--swatch-size);
+    display: block;
+    transform: scale(1);
+    transition: transform var(--fast) var(--transition-in);
+
+    &:hover {
+      transition: none;
+      transform: scale(1.2);
+      z-index: +1;
+    }
+
+    i {
+      font-size: 14px;
+      color: hsl(0, 0%, 100%);
+      filter: saturate(0);
+      mix-blend-mode: difference;
+    }
+    &:nth-last-child(1) {
+      flex-grow: 1;
+    }
+    &:nth-last-child(2) {
+      flex-grow: 1;
+    }
+  }
+}
+</style>
