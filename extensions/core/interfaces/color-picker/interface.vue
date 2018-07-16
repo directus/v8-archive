d5eba6f3d6cd6f03b477fc2081be210f01cd9723
Vincent Kempers <me@vncn.tk>
2018-07-16 18:07:29 +0200

    add color picker interface

---
+++ extensions/core/interfaces/color-picker/interface.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="interface-color-picker">
+      <button
+        v-for="color in colors"
+        v-tooltip="$helpers.formatTitle(color)"
+        :key="color"
+        :style="{ backgroundColor: `var(--${color})`}"
+        :class="{ active: value === color}"
+        @click="$emit('input', color)">
+        <template 
+          v-if="value === color">
+          <i class="material-icons">check</i>
+        </template>
+      </button>
+    </div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+import colors from "./colors.json";
+
+export default {
+  mixins: [mixin],
+  computed: {
+    colors() {
+      return colors;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.interface-color-picker {
+  --swatch-size: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  width: calc(11 * var(--swatch-size));
+
+  button {
+    flex-basis: var(--swatch-size);
+    width: var(--swatch-size);
+    height: var(--swatch-size);
+    display: block;
+    transform: scale(1);
+    transition: transform var(--fast) var(--transition-out);
+    &:hover {
+      transform: scale(2);
+      z-index: +1;
+      transition: transform var(--fast) var(--transition-in);
+    }
+    i {
+      font-size: 14px;
+      color: hsl(0, 0%, 100%);
+      filter: saturate(0);
+      mix-blend-mode: difference;
+    }
+  }
+}
+</style>
