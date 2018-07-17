5af76706a4f65d161ccc41635896505066a218d2
rijkvanzanten <rijkvanzanten@me.com>
2018-07-17 17:15:16 +0200

    Add example page extension

---
+++ extensions/core/pages/testing/page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="example-page">
+    <v-header-bar title="Page Extensions FTW!" />
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "first-page"
+  }
+</script>
+
+<style lang="scss" scoped>
+.example-page {
+  padding: var(--page-padding);
+}
+</style>
