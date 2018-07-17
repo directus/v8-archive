b5120101d440ee72288bcc5e6fd7edd7830fdf12
Vincent Kempers <me@vncn.tk>
2018-07-17 14:44:50 +0200

    create base && array with all icons

---
+++ extensions/core/interfaces/icons/readonly.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+  computed: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
