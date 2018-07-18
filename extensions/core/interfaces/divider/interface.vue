6759981fdd0dffebdf2479f944be9d00d57bd90e
Vincent Kempers <me@vncn.tk>
2018-07-18 19:08:44 +0200

    add base setup for interface

---
+++ extensions/core/interfaces/divider/interface.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+export default {
+  mixin: [mixins],
+  computed: {
+    hello() {
+      console.log("yo");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
