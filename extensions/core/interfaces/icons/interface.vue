eda216fc186ffa84a828d303e8d55481c37b60c5
Vincent Kempers <me@vncn.tk>
2018-07-18 11:46:32 +0200

    deleted comments

--- extensions/core/interfaces/icons/interface.vue
+++ extensions/core/interfaces/icons/interface.vue
@@ -32,20 +32,6 @@
 <script>
 import mixin from "../../../mixins/interface";
 import icons from "./icons.json";
-/*
-@REQUIREMENTS
-It should show every icon in the material icon set that is available for the user.
-The user should search through the icons and when the icon is set, it will be obviously know to the user that the icon is set (active).
-
-@TODO
-V create a scraper for all the material design icons.
-V make an object with all the material design names with value.
-V The object should be created within the group. So every group has it's icons.
-- make an array that has every material design icon in the application.
-- loop through the object and fill the array with the values.
-V use .filter() and .includes() the user input variable.
-V Show every material
-*/
 
 export default {
   mixins: [mixin],
