7577cfee59fc5a85cd5aa4ae06e3faa457b813ce
Vincent Kempers <me@vncn.tk>
2018-07-18 15:51:51 +0200

    add toggle icon interface

---
+++ extensions/core/interfaces/toggle-icon/interface.vue
@@ -0,0 +1,42 @@
+<template>
+	<div v-if="!checkbox">
+		<input
+		type="checkbox"
+		:id="icon-toggle"
+		@change="updateValue($event.target.checked)">
+		<label :for="icon-toggle" :style="{color: `var(--${colorChange})`}">
+			<i class="material-icons">{{ icon }}</i>
+		</label>
+	</div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+  computed: {
+    icon() {
+      return this.value ? this.options.iconActive : this.options.iconInactive;
+    },
+    colorChange() {
+      return this.value ? this.options.colorA : this.options.colorB;
+    }
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
+
+<style type="scss" scoped>
+div {
+  position: relative;
+}
+input {
+  position: absolute;
+  visibility: collapse;
+}
+</style>
---
+++ extensions/core/interfaces/toggle-icon/readonly.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<label :for="icon-toggle" :style="{color: `var(--${colorChange})`}">
+			<i class="material-icons">{{ icon }}</i>
+		</label>
+	</div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+  computed: {
+    icon() {
+      return this.value ? this.options.iconActive : this.options.iconInactive;
+    },
+    colorChange() {
+      return this.value ? this.options.colorA : this.options.colorB;
+    }
+  }
+};
+</script>
+
+<style type="scss" scoped>
+</style>
