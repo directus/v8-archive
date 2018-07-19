7ee19ea8b68ae5502904a72bce846a82800678ff
Vincent Kempers <me@vncn.tk>
2018-07-19 16:12:21 +0200

    add the active text and inactive text to readonly

--- extensions/core/interfaces/toggle-icon/readonly.vue
+++ extensions/core/interfaces/toggle-icon/readonly.vue
@@ -1,7 +1,10 @@
 <template>
+<div>
   <i
     class="material-icons readonly-toggle-icon"
     :style="{color: `var(--${colorChange})`}">{{ icon }}</i>
+    <span :style="{color: `var(--${colorChange})`}">{{ textChange }}</span>
+    </div>
 </template>
 
 <script>
@@ -16,7 +19,20 @@ export default {
     },
     colorChange() {
       return this.value ? this.options.colorActive : this.options.colorInactive;
+    },
+    textChange() {
+      return this.value ? this.options.textActive : this.options.textInactive;
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+div > span {
+  text-transform: uppercase;
+  line-height: 1.18;
+  font-weight: 700;
+  font-size: 0.85em;
+}
+</style>
+
