c96a6ac4d852cdf8ae3e02c5c24608290ae4061a
Vincent Kempers <me@vncn.tk>
2018-07-20 16:41:02 +0200

    add the readonly

--- extensions/core/interfaces/divider/readonly.vue
+++ extensions/core/interfaces/divider/readonly.vue
@@ -1,5 +1,8 @@
 <template>
-    
+    <div :class="`line-style-${options.style} ${options.weight}`">
+        <hr />
+        <span v-if="options.input">{{options.input}}</span>
+    </div>
 </template>
 
 <script>
@@ -7,6 +10,107 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.line-style {
+  position: relative;
+}
+
+// weights
+.hairline > hr {
+  border-top: 1px solid var(--lighter-gray);
+}
+
+.thin > hr {
+  border-top: 2.5px solid var(--lighter-gray);
+}
+
+.medium > hr {
+  border-top: 3.2px solid var(--lighter-gray);
+}
+
+.heavy {
+  hr {
+    border-top: 5px solid var(--lighter-gray);
+  }
+  span:before,
+  span:after {
+    height: 5px;
+  }
+}
+.line-style-standard {
+  span {
+    display: none;
+  }
+}
+
+.line-style-text {
+  display: block;
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+
+  hr {
+    display: none;
+  }
+}
+
+.line-style-text > span {
+  position: relative;
+  text-transform: uppercase;
+  font-size: 0.85em;
+  font-weight: 700;
+  display: inline-block;
+}
+
+.line-style-text > span:before,
+.line-style-text > span:after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  width: 9999px;
+  height: 1px;
+  background: var(--lighter-gray);
+}
+
+.line-style-text > span:before {
+  right: 100%;
+  margin-right: 15px;
+}
+
+.line-style-text > span:after {
+  left: 100%;
+  margin-left: 15px;
+}
+
+.line-style-small {
+  width: 100%;
+  max-width: var(--width-small);
+  margin: 0 auto;
+  span {
+    display: none;
+  }
+}
+
+.line-style-normal {
+  width: 100%;
+  max-width: var(--width-normal);
+  margin: 0 auto;
+
+  span {
+    display: none;
+  }
+}
+
+.line-style-big {
+  width: 100%;
+  max-width: var(--width-large);
+  margin: 0 auto;
+  span {
+    display: none;
+  }
+}
 </style>
+
+
+
 
 
