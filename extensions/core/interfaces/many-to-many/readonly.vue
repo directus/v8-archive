5b204539dac2bc4108e3c90c38fc86920015d57d
rijkvanzanten <rijkvanzanten@me.com>
2018-07-25 11:41:27 -0600

    Render item count in m2m

--- extensions/core/interfaces/many-to-many/readonly.vue
+++ extensions/core/interfaces/many-to-many/readonly.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="readonly-many-to-many">
+    {{ $tc('item_count', (value || []).length, { count: (value || []).length })}}
   </div>
 </template>
 
 <script>
+import mixin from "../../../mixins/interface";
+
 export default {
-  name: "readonly-many-to-many"
+  name: "readonly-many-to-many",
+  mixins: [mixin]
 };
 </script>
 
