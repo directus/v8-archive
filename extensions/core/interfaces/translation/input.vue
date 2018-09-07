610df8dcd32a39b4b912227d8dbf57910d7c21a2
rijkvanzanten <rijkvanzanten@me.com>
2018-09-06 22:02:22 -0400

    Make translations interface functional

--- extensions/core/interfaces/translation/input.vue
+++ extensions/core/interfaces/translation/input.vue
@@ -2,8 +2,8 @@
   <div v-if="error" class="translation error">
     <p><i class="material-icons">warning</i> {{ $t('interfaces-translation-translation_not_setup') }}</p>
   </div>
-  <div v-else-if="activeLanguage" class="translation">
-    <v-simple-select class="language-select" v-model="activeLanguage">
+  <div v-else-if="activeLanguage" class="translation" :class="{ disabled: newItem }">
+    <v-simple-select :disabled="newItem" class="language-select" v-model="activeLanguage">
       <option v-for="language in languages" :key="language.code" :value="language.code">
         {{ language.name }}
       </option>
@@ -12,13 +12,16 @@
     <hr />
 
     <v-form
+      class="form"
+      :readonly="newItem"
       :key="activeLanguage"
       :fields="relatedFields"
       :values="langValue"
       :collection="relation.collection_many"
       ref="form"
-      @unstage-value="unstageValue"
       @stage-value="stageValue" />
+
+    <p v-if="newItem"><i class="material-icons">help_outline</i> {{ $t('interfaces-translation-save_first') }}</p>
   </div>
 </template>
 
@@ -38,9 +41,19 @@ export default {
       if (!this.options.languagesCollection || !this.options.languagePrimaryKeyField) return true;
       return false;
     },
+    primaryKeyField() {
+      return this.$lodash.find(this.fields, { primary_key: true }).field;
+    },
+    primaryKey() {
+      return this.values[this.primaryKeyField];
+    },
     relatedFields() {
       if (!this.relation) return null;
       return this.relation.collection_many.fields;
+    },
+    languageFields() {
+      if (!this.options.languagesCollection) return null;
+      return this.$store.state.collections[this.options.languagesCollection].fields;
     },
     langValue() {
       if (!this.languages || !this.activeLanguage) return {};
@@ -50,7 +63,7 @@ export default {
     },
     valuesByLang() {
       if (!this.value) return {};
-      return this.$lodash.keyBy(this.value, this.relation.field_many.field);
+      return this.$lodash.keyBy(this.value, this.options.languagePrimaryKeyField);
     }
   },
   created() {
@@ -63,7 +76,7 @@ export default {
         .then(res => res.data)
         .then(languages => {
           this.languages = languages;
-          this.activeLanguage = languages[languages.length - 1][this.options.languagePrimaryKeyField];
+          this.activeLanguage = languages[languages.length - 1][this.$lodash.find(this.languageFields, { primary_key: true }).field];
         });
     },
     stageValue({ field, value }) {
@@ -71,14 +84,15 @@ export default {
         return this.$emit("input", [
           ...(this.value || []),
           {
-            [this.relation.field_many.field]: this.activeLanguage,
+            [this.relation.field_many.field]: this.primaryKey,
+            [this.options.languagePrimaryKeyField]: this.activeLanguage,
             [field]: value
           }
         ]);
       }
 
       return this.$emit("input", this.value.map(translation => {
-        if (translation[this.relation.field_many.field] === this.activeLanguage) {
+        if (translation[this.options.languagePrimaryKeyField] === this.activeLanguage) {
           return {
             ...translation,
             [field]: value
@@ -87,9 +101,6 @@ export default {
 
         return translation;
       }));
-    },
-    unstageValue() {
-
     }
   }
 }
@@ -101,6 +112,30 @@ export default {
   padding: var(--page-padding);
   border: var(--input-border-width) solid var(--lighter-gray);
   border-radius: var(--border-radius);
+
+  &.disabled {
+    position: relative;
+
+    .language-select,
+    hr,
+    .form {
+      user-select: none;
+      pointer-events: none;
+      opacity: 0.2;
+    }
+
+    p {
+      top: 0;
+      left: 0;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 }
 
 hr {
