aadd33e59d79b8a7c73f1f59ef55345b8766fffd
rijkvanzanten <rijkvanzanten@me.com>
2018-09-06 22:10:21 -0400

    Render count on display

--- extensions/core/interfaces/translation/display.vue
+++ extensions/core/interfaces/translation/display.vue
@@ -1,5 +1,13 @@
 <template>
+  <div class="display-translation">
+    {{ $tc('item_count', (value || []).length, { count: (value || []).length })}}
+  </div>
 </template>
 
 <script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+};
 </script>
