e71551da9df91f5771dac5b3fb787040e5696ec1
Harsh Kasagara <mail@theharsh.in>
2018-09-20 08:47:23 +1200

    Rating Interface (#414)
    * Button Group Added
    
    * Removed unused code & comments
    
    * Auto-fix linter warnings
    
    * Delete package-lock.json
    
    * Added Choice Grouping Support | Added Theme Support (Solid, Outline)
    
    * Added closing tag
    
    * Rating Interface Added
    
    * Rating Interface Added
    
    * Changed display.vue to show stars | Added 'display' option to define how to display rating values | Removed opacity
    
    * Make number mode the default

---
+++ extensions/core/interfaces/rating/display.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="display-rating">
+    <!-- If 'number' is selected for display option -->
+    <div class="rating-numbers" v-if="options.display=='number'">
+      <i class="material-icons">star</i>
+      <span>{{value}}</span>
+    </div>
+
+    <!-- If 'star' is selected for display option -->
+    <div class="rating-numbers" v-if="options.display=='star'">
+      <stars :options="options" :rating="value" :readonly="true"></stars>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+  components: {
+    Stars: require("./stars.vue").default
+  }
+};
+</script> 
+
+<style lang="scss" scoped>
+.rating-numbers {
+  display: flex;
+  align-items: center;
+  i.material-icons {
+    font-size: 18px;
+    line-height: 1;
+    margin-right: 4px;
+  }
+}
+</style>
