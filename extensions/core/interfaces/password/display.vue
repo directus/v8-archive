18029b9dfcc8a71da89003eac6ca98211328a2ca
Ben Haynes <ben@rngr.org>
2018-09-20 16:13:10 -0400

    Password interface – WIP
    Needs a way to force validation if passwords don’t match

---
+++ extensions/core/interfaces/password/display.vue
@@ -0,0 +1,24 @@
+<template>
+  <i class="material-icons readonly-password" :style="{color: `var(--${colorChange})`}">{{ icon }}</i>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  name: "readonly-password",
+  mixins: [mixin],
+  computed: {
+    icon() {
+      return this.value ? "lock_outline" : "lock_open";
+    },
+    colorChange() {
+      return this.value ? "dark-gray" : "warning";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
