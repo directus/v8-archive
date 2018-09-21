0e0a46c24c31b047617bb96d002ee6b31cdad2aa
Ben Haynes <ben@rngr.org>
2018-09-21 12:18:57 -0400

    WIP file preview interface

---
+++ extensions/core/interfaces/file/display.vue
@@ -0,0 +1,15 @@
+<template>
+  <i class="material-icons">file</i>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin]
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
