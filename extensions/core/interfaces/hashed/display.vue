c7d92aeb30ad71a08675a323b9b479c613400121
Ben Haynes <ben@rngr.org>
2018-09-21 15:06:48 -0400

    Hashed display

--- extensions/core/interfaces/hashed/display.vue
+++ extensions/core/interfaces/hashed/display.vue
@@ -1,3 +1,24 @@
 <template>
-  <i class="material-icons">lock</i>
+  <i class="material-icons readonly-hashed" :style="{color: `var(--${colorChange})`}">{{ icon }}</i>
 </template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  name: "readonly-hashed",
+  mixins: [mixin],
+  computed: {
+    icon() {
+      return this.value ? "lock_outline" : "lock_open";
+    },
+    colorChange() {
+      return this.value ? "dark-gray" : "warning";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
