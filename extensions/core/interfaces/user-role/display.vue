8757ec937f8a23f041cdb459999e1d6659faa1c2
rijkvanzanten <rijkvanzanten@me.com>
2018-10-13 14:07:17 -0400

    Add user-role interface

---
+++ extensions/core/interfaces/user-role/display.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ displayValue }}</div>
+</template>
+
+<script>
+import mixin from "../../../mixins/interface";
+
+export default {
+  mixins: [mixin],
+  name: "display-user-role",
+  computed: {
+    displayValue() {
+      return this.value && this.value[0] && this.value[0].role && this.value[0].role.name;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
