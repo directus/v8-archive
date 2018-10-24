678d60be94e35fcb9ff757fc4597dfd2e2bb5cf5
Welling Guzman <hola@wellingguzman.com>
2018-10-24 15:07:54 -0400

    accept non-throwable in connection exception

--- src/core/Directus/Database/Exception/ConnectionFailedException.php
+++ src/core/Directus/Database/Exception/ConnectionFailedException.php
@@ -3,13 +3,12 @@
 namespace Directus\Database\Exception;
 
 use Directus\Exception\ErrorException;
-use Throwable;
 
 class ConnectionFailedException extends ErrorException
 {
     const ERROR_CODE = 11;
 
-    public function __construct(Throwable $previous = null)
+    public function __construct($previous = null)
     {
         $message = 'Failed to connect to the database';
 
