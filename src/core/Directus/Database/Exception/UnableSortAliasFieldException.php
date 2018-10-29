2fc621307e7352409815024d21946f4e78434c95
Welling Guzman <hola@wellingguzman.com>
2018-10-29 15:17:01 -0400

    unable to sort alias is temporary
    This exception is temporary, there's not need to assigned a dedicated error code just to be removed later on

--- src/core/Directus/Database/Exception/UnableSortAliasFieldException.php
+++ src/core/Directus/Database/Exception/UnableSortAliasFieldException.php
@@ -6,8 +6,6 @@ use Directus\Exception\UnprocessableEntityException;
 
 class UnableSortAliasFieldException extends UnprocessableEntityException
 {
-    const ERROR_CODE = 310;
-
     public function __construct($field)
     {
         parent::__construct(sprintf('Unable to sort by "%s" field because it is an alias.', $field));
