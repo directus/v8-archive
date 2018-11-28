2487811334345622ac040ab38ea70b6b823b56ff
Welling Guzman <hola@wellingguzman.com>
2018-11-28 12:02:22 -0400

    update all composer.json

--- bin/build_subtree.sh
+++ bin/build_subtree.sh
@@ -6,14 +6,11 @@ git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Collec
 # Config
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Config:git@github.com:directus/directus-config.git
 
-# Exception
-git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Exception:git@github.com:directus/directus-php-exception.git
-
-# Permissions
-git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Permissions:git@github.com:directus/directus-permissions.git
-
 # Database
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Database:git@github.com:directus/directus-database.git
+
+# Exception
+git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Exception:git@github.com:directus/directus-php-exception.git
 
 # Filesystem
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Filesystem:git@github.com:directus/directus-filesystem.git
@@ -21,13 +18,16 @@ git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Filesy
 # Hash
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Hash:git@github.com:directus/directus-hash.git
 
+# Helpers
+git subsplit publish --heads="master" --no-tags --debug src/helpers:git@github.com:directus/directus-php-helpers.git
+
 # Hooks
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Hook:git@github.com:directus/directus-hook.git
 
+# Permissions
+git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Permissions:git@github.com:directus/directus-permissions.git
+
 # Utils
 git subsplit publish --heads="master" --no-tags --debug src/core/Directus/Util:git@github.com:directus/directus-php-utils.git
-
-# Helpers
-git subsplit publish --heads="master" --no-tags --debug src/helpers:git@github.com:directus/directus-php-helpers.git
 
 rm -rf .subsplit
