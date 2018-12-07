00e523dd2fc24782f96c7fd7dda02f1e7c0ab179
Sebastian Kinzlinger <webmarken@gmail.com>
2018-12-07 17:08:01 +0200

    Update input.vue

--- extensions/core/interfaces/map/input.vue
+++ extensions/core/interfaces/map/input.vue
@@ -282,10 +282,11 @@ export default {
     },
 
     markerSVG() {
+      // Replace # with %23 so svg also works in Firefox
       return `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="${
-        this.accentColor
+        this.accentColor.replace('#', '%23')
       }" stroke-width="1" stroke="${
-        this.darkAccentColor
+        this.darkAccentColor.replace('#', '%23')
       }" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
     },
 
