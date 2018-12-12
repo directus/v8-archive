871a7a1728dda0036406fa50545b8e5b6c55f3cb
Sebastian Kinzlinger <webmarken@gmail.com>
2018-12-12 15:53:25 +0200

    Custom URL base and modal for existing images
    Added a modal to select and insert existing images. To use it you need to add `"choose-existing"` toolbar option in the field settings.
    This will create a custom button in the toolbar that opens the modal. I also changed the image icon to better distinguish between the upload and select image button.
    
    You can now also set a custom url base for the images in the field settings. If set this url will be used instead of the default S3 or whatever url. Helpful for using IMGIX and the like.

--- extensions/core/interfaces/wysiwyg-full/input.vue
+++ extensions/core/interfaces/wysiwyg-full/input.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="interface-wysiwyg-container">
     <div ref="editor" class="interface-wysiwyg"></div>
+    <portal to="modal" v-if="chooseExisting">
+      <v-modal
+        :title="$t('choose_one')"
+        :buttons="{
+          done: {
+            text: $t('done')
+          }
+        }"
+        @close="chooseExisting = false"
+        @done="chooseExisting = false"
+      >
+        <v-items
+          collection="directus_files"
+          view-type="cards"
+          :selection="[]"
+          :view-options="viewOptions"
+          @select="insertItem($event[0])"
+        ></v-items>
+      </v-modal>
+    </portal>
   </div>
 </template>
 
@@ -27,10 +47,17 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      chooseExisting: false,
+      viewOptions: {title:"title",subtitle:"type",content:"description",src:"data"}
+    }
+  },
   methods: {
     init() {
       let uploadURL = ''
       if (this.options.upload_files) uploadURL = `${this.$store.state.auth.url}/${this.$store.state.auth.project}/files`;
+
       this.editor = new Quill(this.$refs.editor, {
         theme: "snow",
         modules: {
@@ -48,8 +75,9 @@ export default {
               // personalize successful callback and call next function to insert new url to the editor
               callbackOK: (serverResponse, next) => {
                 this.$store.dispatch("loadingFinished", 'uploadingFile');
-                if(typeof serverResponse === 'string') return next(serverResponse);
                 // pass image url to editor
+                if (typeof serverResponse === 'string') return next(serverResponse);
+                else if (this.options.custom_url) return next(`${this.options.custom_url}${serverResponse.data.filename}`);
                 next(serverResponse.data.data.full_url);
               },
               // personalize failed callback
@@ -73,6 +101,28 @@ export default {
       this.editor.on("text-change", () => {
         this.$emit("input", this.editor.root.innerHTML);
       });
+
+      // Make custom icons for image buttons
+      const customButton = document.querySelector('.ql-choose-existing');
+      if(customButton) {
+        customButton.className += " material-icons icon";
+        customButton.addEventListener('click', evt => this.openModal());
+      }
+      const imageButton = document.querySelector('.ql-image');
+      if(imageButton) {
+        imageButton.innerHTML = ""
+        imageButton.className += " material-icons icon";
+      }
+    },
+    openModal() {
+        this.chooseExisting = true
+    },
+    insertItem(image) {
+      let url = image.data.full_url
+      if(this.options.custom_url) url = `${this.options.custom_url}${image.filename}`
+      const index = (this.editor.getSelection() || {}).index || this.editor.getLength();
+      this.editor.insertEmbed(index, 'image', url, 'user');
+      this.chooseExisting = false
     }
   }
 };
@@ -81,5 +131,31 @@ export default {
 <style lang="scss">
 .interface-wysiwyg-container {
   max-width: var(--width-x-large);
+}
+.material-icons {
+  font-size: 20px;
+}
+.ql-choose-existing {
+  padding: 3px 5px;
+  color:  var(--light-gray);
+  &:hover {
+    color: var(--accent);
+  }
+  &:after {
+    content: "collections";
+    font-size: 20px;
+  }
+}
+
+.ql-image {
+  padding: 3px 5px;
+  color:  var(--light-gray);
+  &:hover {
+    color: var(--accent);
+  }
+  &:after {
+    content: "add_photo_alternate";
+    font-size: 20px;
+  }
 }
 </style>
