678b78892376b658cf6e7079eee96c93d3162557
rijkvanzanten <rijkvanzanten@me.com>
2018-12-19 10:52:25 -0500

    Tweak code styling

--- extensions/core/interfaces/wysiwyg-full/input.vue
+++ extensions/core/interfaces/wysiwyg-full/input.vue
@@ -28,9 +28,9 @@
 import Quill from "quill";
 import "quill/dist/quill.core.css";
 import "./quill.theme.css";
-import { ImageUpload } from 'quill-image-upload';
+import { ImageUpload } from "quill-image-upload";
 
-Quill.register('modules/imageUpload', ImageUpload);
+Quill.register("modules/imageUpload", ImageUpload);
 
 import mixin from "../../../mixins/interface";
 
@@ -50,50 +50,71 @@ export default {
   data() {
     return {
       chooseExisting: false,
-      viewOptions: {title:"title",subtitle:"type",content:"description",src:"data"}
-    }
+      viewOptions: {
+        title: "title",
+        subtitle: "type",
+        content: "description",
+        src: "data"
+      }
+    };
   },
   methods: {
     init() {
-      let uploadURL = ''
-      if (this.options.upload_files) uploadURL = `${this.$store.state.auth.url}/${this.$store.state.auth.project}/files`;
+      let uploadURL = null;
+
+      if (this.options.upload_files) {
+        uploadURL = `${this.$store.state.auth.url}/${
+          this.$store.state.auth.project
+        }/files`;
+      }
 
       this.editor = new Quill(this.$refs.editor, {
         theme: "snow",
         modules: {
-          toolbar: this.options.toolbarOptions,
+          toolbar:
+            typeof this.options.toolbarOptions === "string"
+              ? JSON.parse(this.options.toolbarOptions)
+              : this.options.toolbarOptions,
           imageUpload: {
-              url: uploadURL, // server url. If the url is empty then the base64 returns
-              method: 'POST', // change query method, default 'POST'
-              name: 'image', // custom form name
-              withCredentials: false, // withCredentials
-              headers: {
-                Authorization: `Bearer ${this.$store.state.auth.token}`
-              }, // add custom headers, example { token: 'your-token'}
-              csrf: { token: 'token', hash: '' }, // add custom CSRF
-              customUploader: null, // add custom uploader
-              // personalize successful callback and call next function to insert new url to the editor
-              callbackOK: (serverResponse, next) => {
-                this.$store.dispatch("loadingFinished", 'uploadingFile');
-                // pass image url to editor
-                if (typeof serverResponse === 'string') return next(serverResponse);
-                else if (this.options.custom_url) return next(`${this.options.custom_url}${serverResponse.data.filename}`);
-                next(serverResponse.data.data.full_url);
-              },
-              // personalize failed callback
-              callbackKO: serverError => {
-                alert(serverError);
-              },
-              // optional
-              // add callback when a image have been chosen
-              checkBeforeSend: (file, next) => {
-                this.$store.dispatch("loadingStart", {
-                  id: 'uploadingFile'
-                });
-                next(file); // go back to component and send to the server
+            // server url. If the url is empty then the base64 returns
+            url: uploadURL,
+
+            // custom form name
+            name: "image",
+            withCredentials: false,
+            headers: {
+              Authorization: `Bearer ${this.$store.state.auth.token}`
+            },
+            csrf: { token: "token", hash: "" }, // add custom CSRF
+            customUploader: null, // add custom uploader
+            // personalize successful callback and call next function to insert new url to the editor
+            callbackOK: (serverResponse, next) => {
+              this.$store.dispatch("loadingFinished", "uploadingFile");
+              // pass image url to editor
+              if (typeof serverResponse === "string") {
+                return next(serverResponse);
+              } else if (this.options.custom_url) {
+                return next(
+                  `${this.options.custom_url}${serverResponse.data.filename}`
+                );
               }
+
+              return next(serverResponse.data.data.full_url);
+            },
+            // personalize failed callback
+            callbackKO: serverError => {
+              alert(serverError);
+            },
+            // optional
+            // add callback when a image have been chosen
+            checkBeforeSend: (file, next) => {
+              this.$store.dispatch("loadingStart", {
+                id: "uploadingFile"
+              });
+              next(file); // go back to component and send to the server
             }
           }
+        }
       });
 
       this.editor.clipboard.dangerouslyPasteHTML(this.value);
@@ -103,26 +124,28 @@ export default {
       });
 
       // Make custom icons for image buttons
-      const customButton = document.querySelector('.ql-choose-existing');
-      if(customButton) {
+      const customButton = document.querySelector(".ql-choose-existing");
+      if (customButton) {
         customButton.className += " material-icons icon";
-        customButton.addEventListener('click', evt => this.openModal());
+        customButton.addEventListener("click", evt => this.openModal());
       }
-      const imageButton = document.querySelector('.ql-image');
-      if(imageButton) {
-        imageButton.innerHTML = ""
+      const imageButton = document.querySelector(".ql-image");
+      if (imageButton) {
+        imageButton.innerHTML = "";
         imageButton.className += " material-icons icon";
       }
     },
     openModal() {
-        this.chooseExisting = true
+      this.chooseExisting = true;
     },
     insertItem(image) {
-      let url = image.data.full_url
-      if(this.options.custom_url) url = `${this.options.custom_url}${image.filename}`
-      const index = (this.editor.getSelection() || {}).index || this.editor.getLength();
-      this.editor.insertEmbed(index, 'image', url, 'user');
-      this.chooseExisting = false
+      let url = image.data.full_url;
+      if (this.options.custom_url)
+        url = `${this.options.custom_url}${image.filename}`;
+      const index =
+        (this.editor.getSelection() || {}).index || this.editor.getLength();
+      this.editor.insertEmbed(index, "image", url, "user");
+      this.chooseExisting = false;
     }
   }
 };
@@ -137,7 +160,7 @@ export default {
 }
 .ql-choose-existing {
   padding: 3px 5px;
-  color:  var(--light-gray);
+  color: var(--light-gray);
   &:hover {
     color: var(--accent);
   }
@@ -149,7 +172,7 @@ export default {
 
 .ql-image {
   padding: 3px 5px;
-  color:  var(--light-gray);
+  color: var(--light-gray);
   &:hover {
     color: var(--accent);
   }
