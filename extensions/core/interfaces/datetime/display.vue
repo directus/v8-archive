c45f6945c21e5b40ea24cd275795aa8ac2e75977
Rijk van Zanten <rijkvanzanten@me.com>
2019-01-22 11:48:51 -0500

    Respect UTC option on datetime display
    Closes https://github.com/directus/app/issues/1179

--- extensions/core/interfaces/datetime/display.vue
+++ extensions/core/interfaces/datetime/display.vue
@@ -18,11 +18,35 @@ export default {
   computed: {
     date() {
       if (!this.value) return null;
-      return new Date(this.value);
+
+      let date = this.value;
+
+      if (this.options.utc) {
+        date = this.value.includes("T")
+          ? this.value.substring(0, 16)
+          : this.toDatetimeLocal(new Date(this.value.replace(" ", "T") + "Z"));
+      }
+
+      return new Date(date);
     },
     displayValue() {
       if (!this.date) return;
       return this.$d(this.date, "long");
+    }
+  },
+
+  methods: {
+    toDatetimeLocal(date) {
+      const yyyy = date.getFullYear();
+      const mm = this.ten(date.getMonth() + 1);
+      const dd = this.ten(date.getDate());
+      const hh = this.ten(date.getHours());
+      const ii = this.ten(date.getMinutes());
+      const ss = this.ten(date.getSeconds());
+      return `${yyyy}-${mm}-${dd}T${hh}:${ii}:${ss}`;
+    },
+    ten(num) {
+      return String(num).padStart(2, 0);
     }
   }
 };
