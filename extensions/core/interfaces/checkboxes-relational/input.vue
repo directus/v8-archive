0f267452cd98a8cb49e5b916e2b18e8d31013a16
Harsh <mail@theharsh.in>
2019-02-08 20:09:45 +0530

    Add & Delete Completed. Save and Stay Breaking

--- extensions/core/interfaces/checkboxes-relational/input.vue
+++ extensions/core/interfaces/checkboxes-relational/input.vue
@@ -4,10 +4,10 @@
       v-for="item in items"
       :id="uid(item)"
       :key="`checkbox_relational_${item.id}`"
-      :value="item[itemPk]"
+      :value="item[relatedPk]"
       :disabled="readonly"
       :label="labelRendered(item)"
-      :checked="selection.includes(item[itemPk])"
+      :checked="selection.includes(item[relatedPk])"
       @change="onSelection(item.id,$event)"
     ></v-checkbox>
   </div>
@@ -22,38 +22,88 @@ export default {
   data() {
     return {
       items: [],
-      selection: []
+      initiallySelected: [],
+      selection: [],
+      pkMap: {}
     };
   },
-  created() {
-    //Find the field name for the junction table entry
-    let field = this.relation.junction.field_many.field;
-    this.selection = this.value.map(item => item[field][this.itemPk]);
-    this.getItems();
+  watch: {
+    value(newValue) {
+      this.setValues();
+    }
   },
   computed: {
     //The primary key of related table
-    itemPk() {
+    relatedPk() {
       let fields = this.relation.junction.collection_one.fields;
       return this.$lodash.find(fields, {
         primary_key: true
       }).field;
+    },
+
+    //The column name of junction table in ref to related table
+    junctionFieldOfRelated() {
+      return this.relation.junction.field_many.field;
+    },
+
+    //Junction Table Primary Key
+    //? In normal case it would be "ID" only!
+    junctionPk() {
+      return this.$lodash.find(this.relation.collection_many.fields, {
+        primary_key: true
+      }).field;
     }
   },
+  created() {
+    //Get all the items
+    this.getItems();
+
+    //Select the items
+    this.selection = this.value.map(
+      item => item[this.junctionFieldOfRelated][this.relatedPk]
+    );
+
+    //Copy the initial values to seperate array
+    //To make final list when updating
+    this.initiallySelected = this.selection.slice(0);
+
+    this.setValues();
+  },
+
   methods: {
     uid(item) {
       return this.$helpers.shortid.generate() + "_" + item.id;
     },
 
+    // Get items to render the checkboxes
+    getItems() {
+      //Get the collection name of the related table
+      let collection = this.relation.junction.collection_one.collection;
+      this.$api.getItems(collection, {}).then(res => {
+        this.items = res.data;
+      });
+    },
+
+    setValues() {
+      //JunctionKeyVsRelatedKey
+      this.value.forEach(item => {
+        if (!item.$delete) {
+          let relatedPk = item[this.junctionFieldOfRelated][this.relatedPk];
+          let junctionPk = item[this.junctionPk];
+          if (junctionPk) {
+            this.pkMap[relatedPk] = junctionPk;
+          }
+        }
+      });
+    },
+
     labelRendered(val) {
       if (this.options.template) {
-        //Find the field name for the junction table entry
-        let field = this.relation.junction.field_many.field;
         return this.$helpers.micromustache.render(this.options.template, {
-          [field]: val
+          [this.junctionFieldOfRelated]: val
         });
       } else {
-        return this.item[this.itemPk];
+        return this.item[this.relatedPk];
       }
     },
 
@@ -65,27 +115,50 @@ export default {
       } else {
         this.selection.push(val);
       }
-      //Find the field name for the junction table entry
-      let field = this.relation.junction.field_many.field;
-      let selected = this.items
-        .filter(item => this.selection.includes(item.id))
-        .map(item => {
-          return {
-            [field]: item
-          };
-        });
-      console.log(selected);
-      this.$emit("input", selected);
+      this.emitValue(val);
     },
 
-    // Get items to render the checkboxes
-    getItems() {
-      //Get the collection name of the related table
-      let collection = this.relation.junction.collection_one.collection;
-      this.$api.getItems(collection, {}).then(res => {
-        this.items = res.data;
-        console.log("ITEMS", res.data);
+    emitValue(val) {
+      let item = this.items.filter(item => item[this.relatedPk] == val)[0];
+      let newValue;
+
+      //If the item is checked
+      let isSelected = this.selection.includes(item[this.relatedPk]);
+      //If the item is not intitially selected
+      let isInitiallySelected = this.initiallySelected.includes(
+        item[this.relatedPk]
+      );
+
+      //Add New Item
+      if (isSelected && !isInitiallySelected) {
+        newValue = {
+          [this.junctionFieldOfRelated]: item
+        };
+      }
+      //Delete Item
+      else if (!isSelected && isInitiallySelected) {
+        newValue = {
+          [this.junctionPk]: this.pkMap[item[this.junctionPk]],
+          $delete: true
+        };
+      }
+      // //Keep item as it is
+      // else if (isSelected && isInitiallySelected) {
+      //   newValue = {
+      //     [this.junctionPk]: this.pkMap[item[this.junctionPk]],
+      //     [this.junctionFieldOfRelated]: item
+      //   };
+      // }
+
+      let toEmit = this.value.filter(item => {
+        if (item[this.relatedPk] == val) {
+          return newValue;
+        } else {
+          return item;
+        }
       });
+      console.log(toEmit);
+      this.$emit("input", toEmit);
     }
   }
 };
