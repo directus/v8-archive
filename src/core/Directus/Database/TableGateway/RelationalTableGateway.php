3d102bd4782e0c33eb1bb48ae2aa849f93e9c0d3
César Valadez <cesasolvg@gmail.com>
2019-02-28 01:14:01 -0600

    Allowing string relations

--- src/core/Directus/Database/TableGateway/RelationalTableGateway.php
+++ src/core/Directus/Database/TableGateway/RelationalTableGateway.php
@@ -1887,7 +1887,8 @@ class RelationalTableGateway extends BaseTableGateway
             foreach ($entries as &$parentRow) {
                 if (array_key_exists($relationalColumnName, $parentRow)) {
                     // @NOTE: Not always will be a integer
-                    $foreign_id = (int)$parentRow[$relationalColumnName];
+                    // @NOTE: But what about UUIDS and slugs?
+                    $foreign_id = (string)$parentRow[$relationalColumnName];
                     $parentRow[$relationalColumnName] = null;
                     // "Did we retrieve the foreign row with this foreign ID in our recent query of the foreign table"?
                     if (array_key_exists($foreign_id, $relatedEntries)) {
