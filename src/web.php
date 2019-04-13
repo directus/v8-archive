2ca4024b139984204a4b87e5db8fe36b27b47e82
dbenkert <mail@dirk-benkert.de>
2019-04-13 12:37:53 +0200

    added check for maintenance mode with HTTP503 and error message.

--- src/web.php
+++ src/web.php
@@ -35,6 +35,19 @@ if (!file_exists($configFilePath)) {
     exit;
 }
 
+$maintenanceFlagPath = \Directus\create_maintenanceflag_path($basePath);
+if (file_exists($maintenanceFlagPath)) {
+    http_response_code(503);
+    header('Content-Type: application/json');
+    echo json_encode([
+        'error' => [
+            'error' => 21,
+            'message' => 'API is currently under maintenance.'
+        ]
+    ]);
+    exit;
+}
+
 $app = \Directus\create_app($basePath, require $configFilePath);
 
 // ----------------------------------------------------------------------------
