cebba52eafb59164b227cfd5ac8c9a3f92c0c1f9
Binal Gajjar <binal.gajjar7@gmail.com>
2019-07-11 15:44:40 +0530

    Issue Fix #1072 (#1091)
    * Issue Fix 1072
    
    * Fix #1072
    
    * Add condition for add existing data
    
    * Empty check for relational fields
    
    * Added required comments

--- src/core/Directus/Database/TableGateway/RelationalTableGateway.php
+++ src/core/Directus/Database/TableGateway/RelationalTableGateway.php
@@ -607,8 +607,8 @@ class RelationalTableGateway extends BaseTableGateway
                 continue;
             }
 
-            // Update/Add foreign record
-            if ($this->recordDataContainsNonPrimaryKeyData($foreignRow, $foreignTableSchema->getPrimaryKeyName()) && ($field->getType() != "file" && !isset($parentRow[$fieldName][$primaryKey]))) {
+            // TODO: Remove the hardcode consition of file
+            if ($this->recordDataContainsNonPrimaryKeyData($foreignRow, $foreignTableSchema->getPrimaryKeyName()) && $field->getType() != "file" ) {
                  // NOTE: using manageRecordUpdate instead of addOrUpdateRecordByArray to update related data
                 $foreignRow = $ForeignTable->manageRecordUpdate($foreignTableName, $foreignRow);
             }
