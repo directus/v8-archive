d221ec171802820e860c40e1e6d3358be2e2f7e7
Binal Gajjar <binal.gajjar7@gmail.com>
2019-07-19 16:19:57 +0530

    Issue Fix #1125

--- src/core/Directus/Filesystem/Files.php
+++ src/core/Directus/Filesystem/Files.php
@@ -258,12 +258,14 @@ class Files
     {
         // When file is uploaded via multipart form data then We will get object of Slim\Http\UploadFile
         // When file is uploaded via URL (Youtube, Vimeo, or image link) then we will get base64 encode string.
+        $size = null;
         if (is_object($fileData)) {
-
+            $size = $fileData->getSize();
             $checksum = hash_file('md5', $fileData->file);
         } else {
             $fileData = base64_decode($this->getDataInfo($fileData)['data']);
             $checksum = md5($fileData);
+            $size = strlen($fileData);
         }
         // @TODO: merge with upload()
         $fileName = $this->getFileName($fileName, $replace !== true);
@@ -272,9 +274,9 @@ class Files
 
 
 
-        $this->emitter->run('file.save', ['name' => $fileName, 'size' => strlen($fileData)]);
+        $this->emitter->run('file.save', ['name' => $fileName, 'size' => $size]);
         $this->write($fileName, $fileData, $replace);
-        $this->emitter->run('file.save:after', ['name' => $fileName, 'size' => strlen($fileData)]);
+        $this->emitter->run('file.save:after', ['name' => $fileName, 'size' => $size]);
 
         unset($fileData);
 
@@ -685,12 +687,10 @@ class Files
             $dataInfo = $this->getLink($data);
             $result['mimeType']=$dataInfo['type'];
             $result['size']=$dataInfo['filesize'] ? $dataInfo['filesize'] : $dataInfo['size'];
-        }
-        if(is_object($data)) {
+        }else if(is_object($data)) {
             $result['mimeType']=$data->getClientMediaType();
             $result['size']=$data->getSize();
-        }
-        if(strpos($data, 'data:') === 0){
+        }else if(strpos($data, 'data:') === 0){
             $parts = explode(',', $data);
             $file = $parts[1];
             $dataInfo = $this->getFileInfoFromData(base64_decode($file));
