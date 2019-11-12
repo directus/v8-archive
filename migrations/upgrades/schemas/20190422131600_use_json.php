f07a1ece5ad29f551bab14645ec70f7861986511
Binal Gajjar <binal.gajjar7@gmail.com>
2019-11-12 23:44:58 +0530

    Revert back the activity seen field from upgrades

--- migrations/upgrades/schemas/20190422131600_use_json.php
+++ migrations/upgrades/schemas/20190422131600_use_json.php
@@ -15,7 +15,14 @@ class UseJson extends AbstractMigration
             ['collection' => 'directus_activity', 'interface' => 'code']
         ));
 
-  
+        $this->execute(\Directus\phinx_update(	  
+            $this->getAdapter(),	
+            'directus_fields',	
+            [	
+              'interface' => 'json'	
+            ],	
+            ['collection' => 'directus_activity_seen', 'interface' => 'code']	
+        ));
 
         $this->execute(\Directus\phinx_update(
             $this->getAdapter(),
