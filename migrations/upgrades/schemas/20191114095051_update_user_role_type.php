5e48f342ca597d6a476a6769852cbfb0937fe13f
Binal Gajjar <binal.gajjar7@gmail.com>
2019-11-15 16:14:06 +0530

    If exist condition

--- migrations/upgrades/schemas/20191114095051_update_user_role_type.php
+++ migrations/upgrades/schemas/20191114095051_update_user_role_type.php
@@ -28,7 +28,7 @@ class UpdateUserRoleType extends AbstractMigration
      */
     public function change()
     {
-        
+
         $this->execute(\Directus\phinx_update(
             $this->getAdapter(),
             'directus_fields',
@@ -54,7 +54,15 @@ class UpdateUserRoleType extends AbstractMigration
             $this->execute('DELETE FROM `directus_fields` where `collection` = "directus_user_roles";');
         }
 
-        $this->execute('ALTER TABLE `directus_users` ADD COLUMN `role` INT;');
+        $config = \Directus\Application\Application::getInstance()->getContainer()->get('config');
+        $dbName = $config->get('database.name');
+
+
+        $result = $this->query('SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = "'.$dbName.'" AND TABLE_NAME = "directus_users" AND COLUMN_NAME = "role"')->fetch();
+        
+        if(!$result){
+            $this->execute('ALTER TABLE `directus_users` ADD COLUMN `role` INT;');
+        }
 
         $result = $this->query('SELECT 1 FROM `directus_relations` WHERE `collection_many` = "directus_user_roles";')->fetch();
 
@@ -62,11 +70,15 @@ class UpdateUserRoleType extends AbstractMigration
             $this->execute('DELETE FROM `directus_relations` where `collection_many` = "directus_user_roles";');
         }
 
-        $this->execute("INSERT INTO `directus_relations` (`collection_many`, `field_many`, `collection_one`, `field_one`) VALUES ('directus_users', 'role','directus_roles','users');");
-   
-        $stmt = $this->query("SELECT * FROM `directus_user_roles`");
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            $this->execute('UPDATE `directus_users` SET `role` = '.$row['role'].' where id = '.$row['user'].';');
+        $this->execute("INSERT INTO `directus_relations` (`collection_many`, `field_many`, `collection_one`, `field_one`) VALUES ('directus_users', 'role','directus_roles','users');");  
+       
+        $tableExist = $this->query('SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = "'.$dbName.'" AND TABLE_NAME = "directus_user_roles"')->fetch();
+
+        if($tableExist){
+            $stmt = $this->query("SELECT * FROM `directus_user_roles`");
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $this->execute('UPDATE `directus_users` SET `role` = '.$row['role'].' where id = '.$row['user'].';');
+            }
         }
 
         $this->execute('DROP TABLE IF EXISTS directus_user_roles');
