4e2298af6d691848412fbe5e02d976f94c6d935a
rijkvanzanten <rijkvanzanten@me.com>
2019-12-06 15:49:36 -0500

    Use correct system keys on new installations

--- migrations/install/20180220023243_create_settings_table.php
+++ migrations/install/20180220023243_create_settings_table.php
@@ -451,18 +451,46 @@ class CreateSettingsTable extends AbstractMigration
                 'key' => 'asset_whitelist_system',
                 'value' => json_encode([
                     [
-                        "key" => "card",
-                        "width" => 200,
-                        "height" => 200,
-                        "fit" => "crop",
-                        "quality" => 80
+                        'key' => 'directus-small-crop',
+                        'width' => 64,
+                        'height' => 64,
+                        'fit' => 'crop',
+                        'quality' => 80
                     ],
                     [
-                        "key" => "avatar",
-                        "width" => 100,
-                        "height" => 100,
-                        "fit" => "crop",
-                        "quality" => 80
+                        'key' => 'directus-small-contain',
+                        'width' => 64,
+                        'height' => 64,
+                        'fit' => 'contain',
+                        'quality' => 80
+                    ],
+                    [
+                        'key' => 'directus-medium-crop',
+                        'width' => 300,
+                        'height' => 300,
+                        'fit' => 'crop',
+                        'quality' => 80
+                    ],
+                    [
+                        'key' => 'directus-medium-contain',
+                        'width' => 300,
+                        'height' => 300,
+                        'fit' => 'contain',
+                        'quality' => 80
+                    ],
+                    [
+                        'key' => 'directus-large-crop',
+                        'width' => 800,
+                        'height' => 600,
+                        'fit' => 'crop',
+                        'quality' => 80
+                    ],
+                    [
+                        'key' => 'directus-large-contain',
+                        'width' => 800,
+                        'height' => 600,
+                        'fit' => 'contain',
+                        'quality' => 80
                     ]
                 ])
             ]
