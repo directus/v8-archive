17390ecc3e800fe781b65f8faab5ccbf7fda0a9a
Binal Gajjar <binal.gajjar7@gmail.com>
2019-12-20 16:22:35 +0530

    Rename User-Created Interface to Owner

---
+++ migrations/upgrades/20191220102232_update_user_created_to_owner.php
@@ -0,0 +1,40 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class UpdateUserCreatedToOwner extends AbstractMigration
+{
+    public function change()
+    {
+        if ($this->checkFieldExist('directus_files', 'uploaded_by')) {
+            $this->execute(\Directus\phinx_update(
+                $this->getAdapter(),
+                'directus_fields',
+                [
+                    'type' => \Directus\Database\Schema\DataTypes::TYPE_OWNER,
+                    'interface' => 'owner'
+                ],
+                ['collection' => 'directus_files', 'field' => 'uploaded_by']
+            ));
+        }
+
+
+        // To change the type of all the field of user generated collection to owner
+        $this->execute(\Directus\phinx_update(
+            $this->getAdapter(),
+            'directus_fields',
+            [
+                'type' => \Directus\Database\Schema\DataTypes::TYPE_OWNER,
+                'interface' => 'owner'
+            ],
+            ['type' =>  'user_created']
+        ));
+    }
+
+    public function checkFieldExist($collection, $field)
+    {
+        $checkSql = sprintf('SELECT 1 FROM `directus_fields` WHERE `collection` = "%s" AND `field` = "%s";', $collection, $field);
+        return $this->query($checkSql)->fetch();
+    }
+}
