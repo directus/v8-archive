2148908ac24781c44418ba9712669497d4873d8a
jo <ljonas@riseup.net>
2020-02-10 00:11:46 +0100

    Implement db:install

--- src/core/Directus/Console2/Commands/DatabaseInstallCommand.php
+++ src/core/Directus/Console2/Commands/DatabaseInstallCommand.php
@@ -2,6 +2,7 @@
 
 namespace Directus\Console2\Commands;
 
+use Directus\Util\Installation\InstallerUtils;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,14 +16,31 @@ class DatabaseInstallCommand extends AbstractProjectCommand
         parent::configure();
         $this
             ->setDescription('Populate the database')
-            ->addOption('force', 'f', InputOption::VALUE_OPTIONAL, 'Force the process', false)
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Overwrite existing database')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('command not implemented');
+        $basePath = $input->getOption('base-path');
+        $project = $input->getArgument('project-key');
+        $force = $input->getOption('force');
 
-        return 1;
+        $config = InstallerUtils::createApp($basePath, $project)->getConfig();
+
+        $data = [
+            'project' => $project,
+            'db_name' => $config['database']['name'],
+            'db_host' => $config['database']['host'],
+            'db_port' => $config['database']['port'],
+            'db_user' => $config['database']['username'],
+            'db_password' => $config['database']['password'],
+        ];
+
+        InstallerUtils::ensureCanCreateTables($basePath, $data, $force);
+        InstallerUtils::createTables($basePath, $project, $force);
+        InstallerUtils::addUpgradeMigrations($basePath, $project);
+
+        return 0;
     }
 }
