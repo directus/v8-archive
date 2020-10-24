b58ee690a5dded36f3ab10f3006468f9ee316780
everyx <lunt.luo@gmail.com>
2020-10-24 20:28:48 +0800

    Apply Patch #1980
    Performance fix for Response tagging： https://github.com/directus/api/pull/1980/files

--- api/src/core/Directus/Cache/Response.php
+++ api/src/core/Directus/Cache/Response.php
@@ -8,6 +8,14 @@ class Response extends Cache
 
     public function tag($tags)
     {
+        // https://github.com/directus/api/pull/1980/files
+        // array_merge is a performance killer when running in a loop (which this method is often called from)
+		// if the given value is a scalar anyway, we dont need to call array_merge at all
+    	if (is_scalar($tags)) {
+			$this->tags[] = $tags;
+			return $this;
+		}
+
         $this->tags = array_merge($this->tags, (array)$tags);
 
         return $this;
